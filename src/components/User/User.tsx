09def857cd55995f954a1502e32d93bdf055b73d
LeeJS <leejs@LeeJSui-MacBookPro.local>
2019-09-07 17:47:49 +0900

    update User design

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
 import _ from 'lodash';
 import React, { useState } from 'react';
-import { Table, Container, Button } from 'semantic-ui-react';
+import { Table, Container, Button, Icon } from 'semantic-ui-react';
 import { TitleDiv } from '../../app/component/title';
 import { Link } from 'react-router-dom';
 
@@ -11,6 +11,9 @@ interface TableDataIf {
     name: string;
     age: number;
     gender: string;
+    birth: string;
+    joinDate: string;
+    workStatus: boolean;
 }
 
 interface Values {
@@ -31,10 +34,38 @@ const style = {
 
 const User: React.FC = (): JSX.Element => {
     const tableData: TableDataIf[] = [
-        { name: 'John', age: 15, gender: 'Male' },
-        { name: 'Amber', age: 40, gender: 'Female' },
-        { name: 'Leslie', age: 25, gender: 'Other' },
-        { name: 'Ben', age: 70, gender: 'Male' },
+        {
+            name: 'John',
+            age: 15,
+            gender: 'Male',
+            birth: '2019/09/03',
+            joinDate: '2019/09/03',
+            workStatus: true,
+        },
+        {
+            name: 'Amber',
+            age: 40,
+            gender: 'Female',
+            birth: '2019/09/03',
+            joinDate: '2019/09/03',
+            workStatus: true,
+        },
+        {
+            name: 'Leslie',
+            age: 25,
+            gender: 'Other',
+            birth: '2019/09/03',
+            joinDate: '2019/09/03',
+            workStatus: true,
+        },
+        {
+            name: 'Ben',
+            age: 70,
+            gender: 'Male',
+            birth: '2019/09/03',
+            joinDate: '2019/09/03',
+            workStatus: false,
+        },
     ];
 
     const column = null;
@@ -78,34 +109,74 @@ const User: React.FC = (): JSX.Element => {
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell
-                            sorted={column === 'name' ? direction : undefined}
+                            sorted={values.column === 'name' ? direction : undefined}
                             onClick={handleSort}
                             value="name"
+                            textAlign="center"
                         >
-                            Name
+                            名前
                         </Table.HeaderCell>
                         <Table.HeaderCell
-                            sorted={column === 'age' ? direction : undefined}
-                            onClick={handleSort}
-                            value="age"
-                        >
-                            Age
-                        </Table.HeaderCell>
-                        <Table.HeaderCell
-                            sorted={column === 'gender' ? direction : undefined}
+                            sorted={values.column === 'gender' ? direction : undefined}
                             onClick={handleSort}
                             value="gender"
+                            textAlign="center"
                         >
-                            Gender
+                            性別
                         </Table.HeaderCell>
+                        <Table.HeaderCell
+                            sorted={values.column === 'birth' ? direction : undefined}
+                            onClick={handleSort}
+                            value="birth"
+                            textAlign="center"
+                        >
+                            生年月日
+                        </Table.HeaderCell>
+                        <Table.HeaderCell
+                            sorted={values.column === 'age' ? direction : undefined}
+                            onClick={handleSort}
+                            value="age"
+                            textAlign="center"
+                        >
+                            歳
+                        </Table.HeaderCell>
+                        <Table.HeaderCell
+                            sorted={values.column === 'joinDate' ? direction : undefined}
+                            onClick={handleSort}
+                            value="joinDate"
+                            textAlign="center"
+                        >
+                            入社日
+                        </Table.HeaderCell>
+                        <Table.HeaderCell
+                            sorted={values.column === 'workStatus' ? direction : undefined}
+                            onClick={handleSort}
+                            value="workStatus"
+                            textAlign="center"
+                        >
+                            稼動情報
+                        </Table.HeaderCell>
+                        <Table.HeaderCell textAlign="center">詳細</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                    {_.map(values.data, ({ age, gender, name }) => (
+                    {_.map(values.data, ({ age, gender, name, birth, joinDate, workStatus }) => (
                         <Table.Row key={name}>
-                            <Table.Cell>{name}</Table.Cell>
-                            <Table.Cell>{age}</Table.Cell>
-                            <Table.Cell>{gender}</Table.Cell>
+                            <Table.Cell textAlign="center">{name}</Table.Cell>
+                            <Table.Cell textAlign="center">{age}</Table.Cell>
+                            <Table.Cell textAlign="center">{gender}</Table.Cell>
+                            <Table.Cell textAlign="center">{birth}</Table.Cell>
+                            <Table.Cell textAlign="center">{joinDate}</Table.Cell>
+                            <Table.Cell textAlign="center">
+                                {workStatus ? (
+                                    <Icon name="check" color="teal" />
+                                ) : (
+                                    <Icon name="x" color="red" />
+                                )}
+                            </Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Button color="teal" content="詳細" />
+                            </Table.Cell>
                         </Table.Row>
                     ))}
                 </Table.Body>
--- src/components/User/UserAdd.tsx
+++ src/components/User/UserAdd.tsx
@@ -1,18 +1,110 @@
-import React from 'react';
-import { Button, Checkbox, Form, Container, Header, Input } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Form, Container, Icon, Divider, Header } from 'semantic-ui-react';
 import { TitleDiv } from '../../app/component/title';
+import { DateInput, DateInputProps } from 'semantic-ui-calendar-react';
+import moment from 'moment';
+import styled from 'styled-components';
 
-const UserAdd: React.FC = (): JSX.Element => (
-    <Container style={{ minHeight: '89.2vh' }}>
-        <TitleDiv name="User Add" />
-        <Form style={{ marginTop: '14px' }}>
-            <Form.Group widths="equal">
-                <Form.Input fluid label="ID" placeholder="ID" />
-                <Form.Input fluid label="名前" placeholder="名前" />
-            </Form.Group>
-            <Button type="submit">Submit</Button>
-        </Form>
-    </Container>
-);
+const StyleContainer = styled(Container)`
+    .buttonCenter {
+        text-align: center;
+        padding: 12px;
+    }
+`;
+
+const options = [
+    { key: 'm', text: 'Male', value: 'male' },
+    { key: 'f', text: 'Female', value: 'female' },
+];
+
+const UserAdd: React.FC = (): JSX.Element => {
+    const [startDate, setStartDate] = useState();
+    const [birth, setBirth] = useState();
+
+    const changeStartDate = (e: React.SyntheticEvent<HTMLElement>, date: DateInputProps): void => {
+        setStartDate(date.value);
+    };
+
+    const changeBirth = (e: React.SyntheticEvent<HTMLElement>, date: DateInputProps): void => {
+        setBirth(date.value);
+    };
+
+    return (
+        <StyleContainer style={{ minHeight: '89.2vh' }}>
+            <TitleDiv name="User Add" />
+            <Form style={{ marginTop: '14px' }}>
+                <Divider horizontal>
+                    <Header as="h3">
+                        <Icon name="address book outline" />
+                        基本情報
+                    </Header>
+                </Divider>
+
+                <Form.Group widths="equal">
+                    <Form.Input fluid label="ID" placeholder="sample@sample.com" />
+                    <Form.Input fluid label="名前（韓国語）" placeholder="박보검" />
+                    <Form.Input fluid label="名前（漢字）" placeholder="朴寶劍" />
+                    <Form.Input fluid label="名前（フリガナ）" placeholder="パクボゴム" />
+                </Form.Group>
+                <Form.Group widths="equal">
+                    <Form.Select fluid label="Gender" options={options} placeholder="Gender" />
+                    <Form.Field>
+                        <label>生年月日</label>
+                        <DateInput
+                            dateFormat="YYYY/MM/DD"
+                            iconPosition="left"
+                            value={birth || moment().format('YYYY/MM/DD')}
+                            onChange={changeBirth}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>入社日</label>
+                        <DateInput
+                            dateFormat="YYYY/MM/DD"
+                            iconPosition="left"
+                            value={startDate || moment().format('YYYY/MM/DD')}
+                            onChange={changeStartDate}
+                        />
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Input width="3" label="連絡先" placeholder="例) 08000000000" />
+                    <Form.Input width="3" label="郵便番号" placeholder="例) 1690074" />
+                    <Form.Input
+                        width="10"
+                        label="住所"
+                        placeholder="例) 東京都　新宿区　北新宿　1-6-24　朝日ビル別館　201"
+                    />
+                </Form.Group>
+
+                <Divider horizontal>
+                    <Header as="h3">
+                        <Icon name="tty" />
+                        緊急連絡先
+                    </Header>
+                </Divider>
+
+                <Form.Group widths="equal">
+                    <Form.Input fluid label="名前" placeholder="박보검" />
+                    <Form.Input fluid label="家族関係" placeholder="아버지" />
+                    <Form.Input fluid label="連絡先" placeholder="例) 01000000000" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Input width="4" label="郵便番号" placeholder="例) 1690074" />
+                    <Form.Input
+                        width="12"
+                        label="住所"
+                        placeholder="例) 東京都　新宿区　北新宿　1-6-24　朝日ビル別館　201"
+                    />
+                </Form.Group>
+                <div className="buttonCenter">
+                    <Button type="submit" color="teal">
+                        Submit
+                    </Button>
+                </div>
+            </Form>
+        </StyleContainer>
+    );
+};
 
 export default UserAdd;
